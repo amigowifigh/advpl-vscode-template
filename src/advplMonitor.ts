dd4129ee136d9a169bfa04141e770c89207d186d
Luis Ramirez <95533965+amigowifigh@users.noreply.github.com>
2021-12-04 19:25:59 +0000

    Initialize webapp-support

---
+++ src/advplMonitor.ts
@@ -0,0 +1,127 @@
+import * as child_process from 'child_process';
+import * as vscode from 'vscode';
+import { inspect } from 'util';
+import { advplConsole } from './advplConsole';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as nls from 'vscode-nls';
+import * as debugBrdige from  './utils/debugBridge';
+const localize = nls.loadMessageBundle();
+
+export class advplMonitor {
+    private outChannel: advplConsole;
+    private EnvInfos: string;
+    private debugPath: string;
+    private consoleReturn: string;
+
+    constructor(jSonInfos: string, _outChannel: advplConsole) {
+        this.outChannel = _outChannel;
+        this.EnvInfos = jSonInfos;
+        this.debugPath = debugBrdige.getAdvplDebugBridge();
+        
+    }
+
+    public getThreads() {
+        var _args = new Array<string>();
+        var that = this;
+
+        _args.push("--compileInfo=" + this.EnvInfos);
+        _args.push("--threadsInfo");
+
+        var child = child_process.spawn(this.debugPath, _args);
+        child.stdout.on("data", function (data) {
+            var xRet = data + "";
+
+            that.consoleReturn = xRet;
+        });
+
+        child.on("exit", function (data) {
+            that.outChannel.log(that.consoleReturn);
+        });
+    }
+
+    public getRpoInfos(lFunction: boolean) {
+        var _args = new Array<string>();
+        var that = this;
+
+        _args.push("--compileInfo=" + this.EnvInfos);
+        if (lFunction)
+            _args.push("--getFunctions");
+        else
+            _args.push("--getMap");
+
+        this.consoleReturn = "";
+        this.outChannel.log(localize('src.advplMonitor.initializingText', 'Reading the APO, please wait...'));
+        var child = child_process.spawn(this.debugPath, _args);
+
+        child.stdout.on("data", function (data) {
+            var xRet = data + "";
+
+            that.consoleReturn += xRet;
+        });
+
+        child.on("exit", function (data) {
+            that.outChannel.log(localize('src.advplMonitor.creationText', 'Configuration entries were successfully created.'));
+            const newFile = vscode.Uri.parse('untitled:' + path.join(vscode.workspace.rootPath, 'rpoInfo' + (new Date()).getMilliseconds().toString() + '.log'));
+
+            vscode.workspace.openTextDocument(newFile).then(document => {
+                const edit = new vscode.WorkspaceEdit();
+                edit.insert(newFile, new vscode.Position(0, 0), that.consoleReturn);
+
+                return vscode.workspace.applyEdit(edit).then(success => {
+                    if (success) {
+                        vscode.window.showTextDocument(document);
+                    } else {
+                        vscode.window.showInformationMessage(localize('src.advplMonitor.errorText', 'Error!'));
+                    }
+                });
+            });
+        });
+    }
+
+    /**
+     * Gerar o Client do WS
+     */
+    public buildWSClient(): void {
+        var _args = new Array<string>();
+        var that = this;
+        this.consoleReturn = "";
+
+        _args.push("--compileInfo=" + this.EnvInfos);
+        let options: vscode.InputBoxOptions = {
+            prompt: localize('src.advplMonitor.wsQueryUrlText', 'Inform the Web Service URL:')
+        }
+        var urlws = vscode.window.showInputBox(options).then(info => {
+            if (urlws != undefined) {
+                _args.push("--urlBuildWSClient=" + info);
+
+                var child = child_process.spawn(this.debugPath, _args);
+                child.stdout.on("data", function (data) {
+                    that.consoleReturn += "" + data;
+                });
+
+                child.on("exit", function (data) {
+                    that.outChannel.log(localize('src.advplMonitor.wsCreatedText', 'The Web Service was successfully created.'));
+
+                    let initNamePos = that.consoleReturn.indexOf("WSCLIENT") + 9;
+                    let endLine = that.consoleReturn.indexOf("\n", initNamePos);
+                    let name = that.consoleReturn.substr(initNamePos, endLine - initNamePos);
+                    const newFile = vscode.Uri.parse('untitled:' + path.join(vscode.workspace.rootPath, name + 'Client.prw'));
+
+                    vscode.workspace.openTextDocument(newFile).then(document => {
+                        const edit = new vscode.WorkspaceEdit();
+                        edit.insert(newFile, new vscode.Position(0, 0), that.consoleReturn);
+
+                        return vscode.workspace.applyEdit(edit).then(success => {
+                            if (success) {
+                                vscode.window.showTextDocument(document);
+                            } else {
+                                vscode.window.showInformationMessage(localize('src.advplMonitor.errorText', 'Error!'));
+                            }
+                        });
+                    });
+                });
+            }
+        });
+    }
+}
---
+++ src/commands/addAdvplEnvironment.ts
@@ -0,0 +1,127 @@
+'use strict';
+import CodeAdapter from '../adapter';
+import * as vscode from 'vscode';
+import IEnvironment from '../utils/IEnvironment';
+import { advplCompile } from '../advplCompile'
+import * as nls from 'vscode-nls';
+
+const localize = nls.loadMessageBundle();
+
+export default function cmdAddAdvplEnvironment(context): any {
+    const config = vscode.workspace.getConfiguration("advpl");
+    const environments = config.get<Array<IEnvironment>>("environments");
+    let adapter = new CodeAdapter()
+    const questions = [{
+        message: localize('src.commands.addAdvplEnvironment.envAppserverText', 'Environment AppServer'),
+        validate: (env) => {
+            // Comentado pois podem existir cenários onde o usuário possui o mesmo nome de Environment em diferentes serviços.
+            // if (environments.find(environment => environment.environment == env))
+            //     return localize('src.commands.addAdvplEnvironment.envExistsErrorText', 'The inputted environment already exists!');
+            if (env.length <= 0)
+                return localize('src.commands.addAdvplEnvironment.mandatoryText', 'Mandatory!');
+            return true;
+        },
+        name: "environment"
+    }, {
+        message: localize('src.commands.addAdvplEnvironment.nameText', 'Name'),
+        name: "name",
+        when: function (answers) {
+            this.default = answers.environment;
+            return true;
+        },
+        default: '',
+        validate: (name, answers) => {
+
+            /**
+             * Tratamento para evitar duplicidade de environment e erros quando não se é utilizado o atributo name
+             */
+            function trataEnv(environment) {
+                if (environment)
+                    return environment.toUpperCase().trim();
+                else
+                    return environment;
+            }
+
+            // Não permite ambientes com o mesmo Nome (Label)
+            if (environments.find(environment => trataEnv(environment.name) == name.toUpperCase().trim() )){
+                return `${name}` + localize('src.commands.addAdvplEnvironment.existsText', ' already exists!');
+            }
+
+            if (name.length <= 0)
+                return localize('src.commands.addAdvplEnvironment.mandatoryText', 'Mandatory!');
+
+            return true;
+        }
+    }, {
+        message: localize('src.commands.addAdvplEnvironment.smartclientPathText', 'Selecione a pasta do SmartClient'),
+        name: "smartClientPath",
+        type: 'folder'
+    }, {
+        message: localize('src.commands.addAdvplEnvironment.appserverVersionText', 'AppServer Version'),
+        name: "appserverVersion",
+        type: "list",
+        default: '131227A',
+        choices: ['131227A', '170117A']
+    }, {
+        message: localize('src.commands.addAdvplEnvironment.serverIpText', 'Server IP'),
+        name: "server",
+        default: "localhost"
+    }, {
+        message: localize('src.commands.addAdvplEnvironment.appserverPortText', 'AppServer Port'),
+        name: "port",
+        validate: (port, answers) => {
+            // Não permite utilizar um Environment já configurado para o mesmo servidor + serviço (IP + Porta)
+            if (environments.find(env =>
+                env.environment.toUpperCase().trim() == answers.environment.toUpperCase().trim() &&
+                env.server.toUpperCase().trim() == answers.server.toUpperCase().trim() &&
+                env.port.toString() == port.toUpperCase().trim()
+            )) {
+                return answers.environment + localize('src.commands.addAdvplEnvironment.envExistsErrorText', ' already configured for this AppServer');;
+            }
+
+            return true;
+        }
+    }, {
+        message: localize('src.commands.addAdvplEnvironment.userText', 'User'),
+        name: "user",
+        default: "Admin"
+    }, {
+        message: localize('src.commands.addAdvplEnvironment.passwordText', 'Password'),
+        name: "password",
+        type: "password"
+    },
+    {
+        message: localize('src.commands.addAdvplEnvironment.enable', 'Environment Enabled'),
+        name: "enable",
+        type: "list",
+        default: localize('src.extension.yesText', 'Yes'),
+        choices: [localize('src.extension.yesText', 'Yes'), localize('src.extension.noText', 'No')]
+    },
+    {
+        message: localize('src.commands.addAdvplEnvironment.includeListText', 'Selecione as pastas de Include'),
+        name: "includeList",
+        type: 'folder',
+        canSelectMany: true
+    }]
+    adapter.prompt(questions, answers => {
+        const compile = new advplCompile();
+        compile.runCipherPassword(answers.password, cipher => {
+            cipher = cipher.replace(/\r?\n?/g, '')
+            environments.push({
+                environment: answers.environment,
+                name: answers.name,
+                server: answers.server,
+                port: answers.port,
+                serverVersion: answers.appserverVersion,
+                passwordCipher: cipher,
+                includeList: answers.includeList,
+                user: answers.user,
+                smartClientPath: answers.smartClientPath,
+                enable: answers.enable == localize('src.extension.yesText', 'Yes') ? true : false
+
+            });
+            config.update("environments", environments)
+        })
+
+    })
+}
